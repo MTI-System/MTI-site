55daf2a05c053354de5761c22188fe05ac9baf08
Serge46 <serg46b@gmail.com>
2025-07-06 15:46:27 +0700

    Fixed broken styles on rightContainer in header
    Fixed wrong API endpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { FaUserCircle } from "react-icons/fa"
 import headerStyle from "@/styles/app/header.module.css"
 import footerStyle from "@/styles/app/footer.module.css"
 import "@/styles/app/main.css"
-import Link from 'next/link';
+import Link from "next/link"
 
 function RootLayout({
   children,
@@ -17,10 +17,12 @@ function RootLayout({
         <header className={headerStyle.header}>
           <div className={headerStyle.rightContainer}>
             <IoLogoCodepen className={"logo"} />
-            <Link href={"/"}><h2>МТИ</h2></Link>
+            <Link href={"/"}>
+              <h2>МТИ</h2>
+            </Link>
             <input placeholder="Тюф/Тюе" className="search"></input>
           </div>
-          <div className="horizontal">
+          <div className={headerStyle.rightContainer}>
             <input placeholder="Поиск" className="search"></input>
             <FaMoon className="icon-button" />
             <Link href={"/profile"}>
