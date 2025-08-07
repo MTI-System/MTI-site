5ce29c11f17325361555c473f03db7dc31b3178a
Mof1us <58910703+mof1us@users.noreply.github.com>
2025-08-07 18:48:23 +0300

    Рабочий sitemap fix

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://mtiyt/sitemap.xml",
+    sitemap: "https://mtiyt.ru/sitemap.xml",
   }
 }
