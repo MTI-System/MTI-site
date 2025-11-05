41be79fb65c9bd46e446db3bf0b618541a95fb7c
mofius <antoon.s.ivanov@gmail.com>
2025-11-05 12:11:47 +0300

    Набросок конструктора форм в tmp2

--- src/app/(main)/tmp2/page.tsx
+++ src/app/(main)/tmp2/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import {Forms} from "@/components/forms"
 import DatePicker from "@/components/pickers/DatePicker";
+import {ConstructorLayout} from "@/components/formConstructor/ConstructorLayout";
 
 export default function SecondTmp() {
   return (
     <>
-
+      <ConstructorLayout/>
     </>
   )
 }
