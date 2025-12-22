97d2401d267be982bb246d51c9e631a4bb336bfd
Mof1us <antonplay983@gmail.com>
2025-12-22 13:01:37 +0300

    change .env vars

--- src/constants/APIEndpoints.ts
+++ src/constants/APIEndpoints.ts
@@ -1,6 +1,19 @@
+const isServer = typeof window === "undefined";
+
 export const protocol = process.env.NEXT_PUBLIC_IS_ON_DEV === "true" ? `http` : `https`
 export const webSocketProtocol = process.env.NEXT_PUBLIC_IS_ON_DEV === "true" ? `ws` : `wss`
-export const GATEWAY_API = process.env.NEXT_PUBLIC_IS_ON_DEV === "true" ? "://11.0.0.1:5002/" : "://api.mtiyt.ru/"
+export const GATEWAY_API =
+  isServer ?
+    process.env.NEXT_PUBLIC_IS_LOCAL === "true" ?
+      process.env.NEXT_PUBLIC_IS_ON_DEV === "true" ?
+        "://11.0.0.1:5002/"
+        : "://api.mtiyt.ru/"
+      : "://gateway:8080"
+  : process.env.NEXT_PUBLIC_IS_ON_DEV === "true"
+      ? "://11.0.0.1:5002/"
+      : "://api.mtiyt.ru/"
+
+  // process.env.NEXT_PUBLIC_IS_ON_DEV === "true" ? "://11.0.0.1:5002/" : "://api.mtiyt.ru/" // на сервере эта переменная всегда была ://gateway:8080/
 export const AUTH_API = protocol + GATEWAY_API + "auth/"
 export const PROBLEM_API = protocol + GATEWAY_API + "problems/"
 export const FILES_SERVER = protocol + GATEWAY_API + "files/get/"
